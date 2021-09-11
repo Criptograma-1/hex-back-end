1e351304e3dae9433ff6a772e56936f1bc7e4706
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-09-11 17:44:41 -0300

    Fix documentation

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """urllib package"""
+
+
 import requests from urllib
 
 
 if __name__ == "__main__":
-    """script that fetches https://intranet.hbtn.io/status"""
     with requests.urlopen('https://intranet.hbtn.io/status') as response:
-        """Print a response and response type"""
         response = response.read()
         print("Body response:")
         print(" - type: {}".format(type(response)))
