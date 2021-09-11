af08e3f227ec1ddb675707e93e54ee9463ee4fdb
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-09-11 18:07:24 -0300

    Fix documentation

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
 
     """fetches https://intranet.hbtn.io/status"""
     with requests.urlopen('https://intranet.hbtn.io/status') as response:
-        """Python script that fetches https://intranet.hbtn.io/status"""
+        """Print response"""
         html = response.read()
-        """Python script that fetches https://intranet.hbtn.io/status"""
         print('Body response:')
-        """Python script that fetches https://intranet.hbtn.io/status"""
         print('\t- type: {}'.format(type(html)))
-        """Python script that fetches https://intranet.hbtn.io/status"""
         print('\t- content: {}'.format(html))
