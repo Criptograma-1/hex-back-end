2cc4c1e3556300d5d4fb9632f8018f406e216009
Criptograma-1 <andresa.info@gmail.com>
2021-09-11 20:39:25 +0000

    Fix documentation

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
-
 import requests from urllib
 
-"""Print a response and response type"""
+
 if __name__ == "__main__":
+    """Print a response and response type"""
     with requests.urlopen('https://intranet.hbtn.io/status') as response:
         response = response.read()
         print("Body response:")
