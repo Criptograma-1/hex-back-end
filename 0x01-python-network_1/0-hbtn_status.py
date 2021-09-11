c1201b00304cc0dcc28f9eec8ff0c18e43b74a1c
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-09-11 17:53:26 -0300

    Update 0-hbtn_status.py

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -8,10 +8,13 @@ import requests from urllib
 def urlRequest():
     """ Function that makes a request to a url """
     with request.urlopen('https://intranet.hbtn.io/status') as response:
+        """ Function that makes a request to a url """
         response = response.read()
+    """ Function that makes a request to a url """
     print("Body response:")
     print("- type: {}".format(type(response)))
     print("- content: {}".format(response))
 
 if __name__ == "__main__":
+    """ Function that makes a request to a url """
     urlRequest()
