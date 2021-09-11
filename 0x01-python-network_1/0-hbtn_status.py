c3cbc9ff12e604e368e29ebc7d592ec566c175a0
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-09-11 18:01:57 -0300

    Update 0-hbtn_status.py

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
-"""
-Contains a Python script that fetches https://intranet.hbtn.io/status.
-"""
-import requests from urllib
-
-
-def run():
-    """
-    Fetches https://intranet.hbtn.io/status.
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with requests.urlopen(url) as res:
-        r = res.read()
-        print('Body response:')
-        print('\t- type:', type(r))
-        print('\t- content:', r)
-        print('\t- utf8 content:', r.decode('utf-8'))
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    """
-    Fetches https://intranet.hbtn.io/status.
-    """
-    run()
+    import requests from urllib
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
