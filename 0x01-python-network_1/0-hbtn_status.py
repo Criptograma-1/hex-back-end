4b08aa735c9494e195c93804921282dfb646f204
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-09-11 17:51:20 -0300

    Fix code

--- 0x01-python-network_1/0-hbtn_status.py
+++ 0x01-python-network_1/0-hbtn_status.py
@@ -2,13 +2,16 @@
 """
 Write a Python script that fetches https://intranet.hbtn.io/status
 """
-import requests from urllib
+from urllib import request
 
+
+def urlRequest():
+    """ Function that makes a request to a url """
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        response = response.read()
+    print("Body response:")
+    print("- type: {}".format(type(response)))
+    print("- content: {}".format(response))
 
 if __name__ == "__main__":
-    """"Function that makes a request to a url"""
-    with requests.urlopen('https://intranet.hbtn.io/status') as response:
-        response = response.read()
-        print("Body response:")
-        print(" - type: {}".format(type(response)))
-        print(" - content: {}".format(response))
+    urlRequest()
