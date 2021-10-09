4838f7044d3b82b691e3b405183e2bece60bf0d5
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-10-09 20:55:30 -0300

    Fix doc

--- 0x01-python-network_1/6-my_github.py
+++ 0x01-python-network_1/6-my_github.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-# Python script that fetches id from api.github.com/users
+""" Python script that fetches id from api.github.com/users """
+
+import requests as res
+from sys import argv
+
+
 if __name__ == "__main__":
-    import requests as res
-    from sys import argv
     try:
         r = res.get('https://api.github.com/user', auth=(argv[1], argv[2]))
         print(r.json().get('id'))
