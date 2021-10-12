3126d68e3d60b343b8c64c614829645a75eea267
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-10-12 20:34:03 -0300

    PEP8

--- 0x02-api/0-gather_data_from_an_API.py
+++ 0x02-api/0-gather_data_from_an_API.py
@@ -5,31 +5,31 @@ about employee TODO list progress
 """
 
 
-from sys import argv
 import json
 import requests as res
+from sys import argv
 
 
 def getApi(u):
     """A function that returns to do list"""
     user = int(u)
     done = 0
-    todo = res.get('https://jsonplaceholder.typicode.com/todos', 
+    todo = res.get('https://jsonplaceholder.typicode.com/todos',
                    params={'userId': user})
-    em = res.get('https://jsonplaceholder.typicode.com/users', 
+    em = res.get('https://jsonplaceholder.typicode.com/users',
                  params={'id': user})
     empl = json.loads(em.text)
     td = json.loads(todo.text)
 
     for i in range(len(td)):
-        if td[i]['completed'] == True:
+        if td[i]['completed'] is True:
             done = done+1
     undone = len(td)
     name = empl[0]['name']
-    
-    print("Employee {} is done with tasks ({}/{}):".format(name,done,undone))
+
+    print("Employee {} is done with tasks ({}/{}):".format(name, done, undone))
     for i in range(len(td)):
         print("\t{}".format(td[i]['title']))
 
 if __name__ == '__main__':
-     getApi(argv[1])    
+       getApi(argv[1])    
