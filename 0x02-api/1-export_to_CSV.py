3e5f887e31e1cb93052308c731cde802ea8b731c
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-11-13 20:59:33 -0300

    PEP8

--- 0x02-api/1-export_to_CSV.py
+++ 0x02-api/1-export_to_CSV.py
@@ -15,15 +15,15 @@ def getApi(u):
                    params={'id': user}).json()
 
     data_to_file = open(u+".csv", 'w', newline='')
-    csv_writer = csv.writer(data_to_file,quoting=csv.QUOTE_NONNUMERIC)
+    csv_writer = csv.writer(data_to_file, quoting=csv.QUOTE_NONNUMERIC)
 
     for i in range(0, len(todo)):
         todoList = todo[i]
-        id =str(todoList['userId'])
-        name =empl[0]['username']
-        status =str(todoList['completed'])
+        id = str(todoList['userId'])
+        name = empl[0]['username']
+        status = str(todoList['completed'])
         title = todoList['title']
-        csv_writer.writerow([id,name,status,title])
+        csv_writer.writerow([id, name, status, title])
     data_to_file.close()
 
 if __name__ == '__main__':
