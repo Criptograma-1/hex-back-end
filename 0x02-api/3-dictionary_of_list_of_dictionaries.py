5c55659d83b3640ea58a3f01f6655d4f30074801
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-11-15 14:42:33 -0300

    PEP8

--- 0x02-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x02-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,11 +12,9 @@ def getApi():
     filename = open("todo_all_employees.json", 'w')
     for i in range(len(empl)):
         id = empl[i]['id']
-        todo = res.get('https://jsonplaceholder.typicode.com/todos',
-                   params={'userId': id}).json()
+        todo = res.get('https://jsonplaceholder.typicode.com/todos', params={'userId': id}).json()
         todoList = []
         for j in range(len(todo)):
-            
             line = { "task": todo[j]['title'], "completed": todo[j]['completed'], "username": empl[i]['username']}
             todoList.append(line)
         todoDict.update({id: todoList})
