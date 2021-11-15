8d50474bf8b29766fe87b281333e334fa73ea7cf
Criptograma <andresa.info@gmail.com>
2021-11-15 17:39:42 +0000

    Fix task 3

--- 0x02-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x02-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,19 +7,20 @@ import requests as res
 
 def getApi():
     """A function that returns to do list"""
-    empl = (res.get('https://jsonplaceholder.typicode.com/users').json()
-    todoDict = {};
+    empl = res.get('https://jsonplaceholder.typicode.com/users').json()
+    todoDict = {}
     filename = open("todo_all_employees.json", 'w')
-    for i in empl:
-        todoList =[];
-        user = empl[i]['id']
+    for i in range(len(empl)):
+        id = empl[i]['id']
         todo = res.get('https://jsonplaceholder.typicode.com/todos',
-                   params={'userId': user}).json()
+                   params={'userId': id}).json()
+        todoList = []
         for j in range(len(todo)):
+            
             line = { "task": todo[j]['title'], "completed": todo[j]['completed'], "username": empl[i]['username']}
             todoList.append(line)
-        todoDict.update({empl[i]['id']: todoList})
-        print(todoDict)
+        todoDict.update({id: todoList})
+    return json.dump(todoDict, filename)
         
 
 if __name__ == '__main__':
