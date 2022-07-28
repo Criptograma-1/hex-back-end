41f221be4784d0cc33f98385387255f6ec411280
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-28 15:13:36 -0300

    Add files via upload

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from ('mongodb')
+import { MongoClient, Server } from 'mongodb';
 
 class DBClient {
   constructor() {
@@ -6,13 +6,13 @@ class DBClient {
     this.port = process.env.DB_PORT || 27017;
     this.database = process.env.DB_DATABASE || 'files_manager';
 
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`);
-    await this.client.connect();
+    this.client = new MongoClient(new Server(this.host, this.port), { native_parser: true });
+    this.client.connect();
     this.db = this.client.db(this.database);
   }
 
   isAlive() {
-    return this.client.topology.isConnected();
+    return !!this.client.topology && this.client.topology.isConnected();
   }
 
   async nbUsers() {
