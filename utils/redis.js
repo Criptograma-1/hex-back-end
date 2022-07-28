2210a3549f537b719f35abc1e749099744921178
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-28 13:42:11 -0300

    Add files via upload

---
+++ utils/redis.js
@@ -0,0 +1,28 @@
+import {createClient} from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err.message));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value, {EX: duration});
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
