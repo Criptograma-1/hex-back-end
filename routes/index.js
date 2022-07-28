abe2435f4ceb84e95df8fe4ff067861180722093
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2022-07-28 16:16:23 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-
-const express = require('express');
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-module.exports = () => {
-  router.get('/', (req, res) => {
-    res.sendStatus(200);
-  });
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
-  router.get('/status', AppController.getStatus);
-  router.get('/stats', AppController.getStats);
-
-  router.post('/users', UsersController.postNew);
-  router.get('/users/me', UsersController.getMe);
-
-  router.get('/connect', AuthController.getConnect);
-  router.get('/disconnect', AuthController.getDisconnect);
-
-  return router;
-};
+export default router;
